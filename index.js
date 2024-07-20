95465f9fe1adaeb4f5173f36aebea177e7a921ec
CupheadUltra <129087076+CupheadUltra@users.noreply.github.com>
2024-07-20 11:22:00 +0300

    half of tast

---
+++ index.js
@@ -0,0 +1,13 @@
+const age = 13;
+console.log(age);
+const name = "Sviatoslav";
+console.log(name);
+const isStudent = true;
+console.log(isStudent);
+const myString = "'You are filled with determination'";
+console.log(myString);
+const myNumber = 5;
+const number = myNumber + 10;
+console.log(number);
+const myNull = null;
+console.log(myNull);
