37b01c9e73cd82ddc7c36d57b6a51df495331169
CupheadUltra <129087076+CupheadUltra@users.noreply.github.com>
2024-07-20 11:57:08 +0300

    full task

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
-const age = 13;
+
+let age = 13;
 console.log(age);
+
 const name = "Sviatoslav";
 console.log(name);
-const isStudent = true;
+
+let isStudent = true;
 console.log(isStudent);
-const myString = "'You are filled with determination'";
+
+let myString =
+  "'You are filled with determination'";
 console.log(myString);
-const myNumber = 5;
-const number = myNumber + 10;
+
+let myNumber = 5;
+let number = myNumber + 10;
 console.log(number);
+
 const myNull = null;
 console.log(myNull);
+
+let userName = prompt("Введіть ваше ім'я");
+alert("Вітаємо, " + userName);
+
+const confirmAction = confirm("Підтвердіть вашу дію");
+if (confirmAction) {
+  alert("Дякую за підтвердження!");
+} else {
+  alert("Дію відмінено!");
+}
+
+let dangerAction = confirm("Ця дія небезпечна. Ви впевнені?");
+if (dangerAction) {
+  alert("Дію підтвердженно");
+} else {
+  alert("Дію відмінено!");
+}
